dd658cd23b61fd8e6143cd0653eb62366b936a34
AFeliz <AlbertFeliz@larasofts.com>
2024-04-25 15:22:54 -0400

    CAMBIOS

--- js/FDPU.js
+++ js/FDPU.js
@@ -30,3 +30,23 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+  function generarEnlaceWhatsApp() {
+    // Obtener valores de los campos del formulario
+    var nombre = document.getElementById('txtNombre').value;
+    var whatsapp = document.getElementById('txtWhatsApp').value;
+    var email = document.getElementById('txtCorreo').value;
+    var direccion = document.getElementById('txtDireccion').value;
+
+    // Crear el mensaje
+    var mensaje = `Hola, mi nombre es ${nombre}. Mi correo electrónico es ${email} y vivo en ${direccion}. ¿Podemos hablar?`;
+
+    // Codificar el mensaje para URL
+    var mensajeCodificado = encodeURIComponent(mensaje);
+
+    // Generar el enlace de WhatsApp
+    var enlaceWhatsApp = `https://wa.me/${whatsapp}?text=${mensajeCodificado}`;
+
+    // Opcional: Redireccionar al usuario al enlace de WhatsApp
+    window.open(enlaceWhatsApp, '_blank');
+}
+
